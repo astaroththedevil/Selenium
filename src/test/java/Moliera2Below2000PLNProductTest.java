7c59e110799e0de0d0c2a2ed0c4abeb4e2e7e8dc
rafal <astaroththedevil@gmail.com>
2021-09-20 06:23:05 +0200

    first functional Moliera2Above1999PLNProductTest

--- src/test/java/Moliera2Below2000PLNProductTest.java
+++ src/test/java/Moliera2Below2000PLNProductTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Moliera2Above1999PLNProductTest {
+public class Moliera2Below2000PLNProductTest {
 
     WebDriver driver;
 
@@ -30,7 +30,7 @@ public class Moliera2Above1999PLNProductTest {
     }
 
     @Test
-    public void shoulOpenProductAbove1999PLN() throws InterruptedException {
+    public void shouldOpenProductBelow2000PLN() throws InterruptedException {
         driver.get("https://www.moliera2.com/");
         driver.manage().window().maximize();
         Thread.sleep(3000);
@@ -38,11 +38,17 @@ public class Moliera2Above1999PLNProductTest {
         Thread.sleep(4000);
         driver.findElement(By.xpath("//*[@id=\"sm-sidebar\"]/div[1]/p")).click();
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"pjax-content\"]/div/div[3]/div[1]/div[4]/div/div/div[1]/div[1]/div/a/img")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.xpath("/html/body/div[27]/div/div/div/div/button/span")).click();
+        driver.findElement(By.xpath("//*[@id=\"pjax-content\"]/div/div[3]/div[1]/div[4]/div/div/div[3]/div/div/a/img")).click();
         Thread.sleep(3000);
-        driver.findElement(By.xpath("//*[@id=\"product-list\"]/div[3]/a/div[2]/div[1]/h2")).click();
+        driver.findElement(By.xpath("//*[@id=\"product-list\"]/div[2]/a/div[2]/div[1]/h2")).click();
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//*[@id=\"cart-product-form-1825741\"]/div[2]/div[1]/div[1]/div[2]/span[2]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"cart-product-form-1825741\"]/div[2]/div[1]/div[1]/div[3]/ul/li[2]/div")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"cart-product-form-1825741\"]/div[4]/div/div/a")).click();
+        Thread.sleep(3500);
+        driver.findElement(By.xpath("//*[@id=\"goto-cart\"]")).click();
         Thread.sleep(4000);
 
         printSuccess();
