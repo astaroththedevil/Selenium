f0ef8edbfc9b15cc9583d93faeff21e7876eea72
Rafal Sypniewski <r.sypniewski@moliera2.com>
2021-10-27 11:53:33 +0200

    some shit commit

---
+++ src/main/java/SomeShit.java
@@ -0,0 +1,25 @@
+public class SomeShit {
+    private String nameOfShit;
+    private int quantityOfShit;
+
+    public SomeShit(String nameOfShit, int quantityOfShit) {
+        this.nameOfShit = nameOfShit;
+        this.quantityOfShit = quantityOfShit;
+    }
+
+    public String getNameOfShit() {
+        return nameOfShit;
+    }
+
+    public void setNameOfShit(String nameOfShit) {
+        this.nameOfShit = nameOfShit;
+    }
+
+    public int getQuantityOfShit() {
+        return quantityOfShit;
+    }
+
+    public void setQuantityOfShit(int quantityOfShit) {
+        this.quantityOfShit = quantityOfShit;
+    }
+}
